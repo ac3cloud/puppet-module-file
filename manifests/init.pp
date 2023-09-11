fb321f8eda60d59848724b46c66b6dc73ac2d884
Michael Baker <elmobp@users.noreply.github.com>
2023-09-11 09:56:56 +0800

    Update init.pp
    This gone in stdlib long time ago deprecated but finally gone

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@ class file {
   $files = hiera_hash('file::file')
 
   if $files {
-    validate_hash($files)
+    validate_legacy(Hash, 'validate_hash', $files)
     create_resources('file::file', $files, $defaults)
   }
 }
